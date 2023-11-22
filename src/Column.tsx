2a2cf5256f3dabec681ff4a661b44dc9117e4fb4
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-11-22 21:50:28 +0900

    enable to move todo

--- src/Column.tsx
+++ src/Column.tsx
@@ -9,6 +9,8 @@ export function Column({
   title,
   filterValue: rawFilterValue,
   cards: rawCards,
+  onCardDragStart,
+  onCardDrop,
 }: {
   title?: string
   filterValue?: string
@@ -16,6 +18,8 @@ export function Column({
     id: string
     text?: string
   }[]
+  onCardDragStart?(id: string): void
+  onCardDrop?(id: string | null): void
 }) {
   const filterValue = rawFilterValue?.trim()
   const keywords = filterValue?.toLowerCase().split(/\s+/g) ?? []
@@ -29,6 +33,14 @@ export function Column({
   const toggleInput = () => setInputMode(v => !v)
   const confirmInput = () => setText('')
   const cancelInput = () => setInputMode(false)
+
+  const [draggingCardID, setDraggingCardID] = useState<string | undefined>(
+    undefined,
+  )
+  const handleCardDragStart = (id: string) => {
+    setDraggingCardID(id)
+    onCardDragStart?.(id)
+  }
 
   return (
     <Container>
@@ -51,9 +63,31 @@ export function Column({
       {filterValue && <ResultCount>{cards.length} results</ResultCount>}
 
       <VerticalScroll>
-        {cards.map(({ id, text }) => (
-          <Card key={id} text={text} />
+        {cards.map(({ id, text }, i) => (
+          <Card.DropArea
+            key={id}
+            disabled={
+              draggingCardID !== undefined &&
+              (id === draggingCardID || cards[i - 1]?.id === draggingCardID)
+            }
+            onDrop={() => onCardDrop?.(id)}
+          >
+            <Card
+              text={text}
+              onDragStart={() => handleCardDragStart(id)}
+              onDragEnd={() => setDraggingCardID(undefined)}
+            />
+          </Card.DropArea>
         ))}
+
+        <Card.DropArea
+          style={{ height: '100%' }}
+          disabled={
+            draggingCardID !== undefined &&
+            cards[cards.length - 1]?.id === draggingCardID
+          }
+          onDrop={() => onCardDrop?.(null)}
+        />
       </VerticalScroll>
     </Container>
   )
